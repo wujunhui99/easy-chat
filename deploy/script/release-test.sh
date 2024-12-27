40ef9a52102137016f0f112fac7e9c9cfe62fc6b
wujunhui99 <344686925@qq.com>
2024-12-27 14:13:51 +0000

    without_bin

--- deploy/script/release-test.sh
+++ deploy/script/release-test.sh
@@ -1,16 +1,26 @@
+#!/bin/bash
+
 need_start_server_shell=(
   # rpc
-  user-rpc-test.sh
-
+  "user-rpc-test.sh"
   # api
 )
 
-for i in ${need_start_server_shell[*]} ; do
-    chmod +x $i
-    ./$i
+# 确保是在正确的目录下
+cd "$(dirname "$0")"
+
+for i in "${need_start_server_shell[@]}"; do
+    if [ -f "$i" ]; then
+        echo "Executing $i"
+        chmod +x "$i"
+        ./"$i"
+    else
+        echo "Warning: $i not found"
+    fi
 done
 
-
+echo "Checking running containers:"
 docker ps
 
+echo "Checking etcd keys:"
 docker exec -it etcd etcdctl get --prefix ""

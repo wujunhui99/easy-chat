732e1b4d9d6ef5239460b407cb6b12c92d70dad4
wujunhui99 <344686925@qq.com>
2025-03-30 06:28:16 +0000

    e6-2

---
+++ deploy/script/user-rpc-test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+reso_addr='crpi-6zxn5tvxgfe9vkci.cn-shenzhen.personal.cr.aliyuncs.com/easy-chat-junhui/user-rpc-dev'
+tag='latest'
+
+# pod_ip="192.168.117.24"
+pod_ip=$(hostname -I | awk '{print $1}')
+
+container_name="easy-chat-user-rpc-test"
+
+docker stop ${container_name}
+
+docker rm ${container_name}
+
+docker rmi ${reso_addr}:${tag}
+
+docker pull ${reso_addr}:${tag}
+
+
+# 如果需要指定配置文件的
+# docker run -p 10001:8080 --network imooc_easy-im -v /easy-im/config/user-rpc:/user/conf/ --name=${container_name} -d ${reso_addr}:${tag}
+docker run -p 10000:10000 -e POD_IP=${pod_ip}  --name=${container_name} -d ${reso_addr}:${tag}
